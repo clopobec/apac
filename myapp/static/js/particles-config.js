3054b5ba496bbe171ae84161072f094c0bb81257
Nicolas <zeldon02@gmail.com>
2025-02-12 22:28:35 +0100

    ajout navbar

---
+++ myapp/static/js/particles-config.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    particlesJS("particles-js", {
+        "particles": {
+            "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+            "color": { "value": "#1a1a2e" },
+            "shape": { "type": "circle" },
+            "opacity": { "value": 0.5, "random": false },
+            "size": { "value": 3, "random": true },
+            "line_linked": { "enable": true, "distance": 150, "color": "#1a1a2e", "opacity": 0.4, "width": 1 },
+            "move": { "enable": true, "speed": 2, "direction": "none", "straight": false }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": { "enable": true, "mode": "repulse" },
+                "onclick": { "enable": true, "mode": "push" }
+            },
+            "modes": {
+                "repulse": { "distance": 100, "duration": 0.4 },
+                "push": { "particles_nb": 4 }
+            }
+        }
+    });
+});
